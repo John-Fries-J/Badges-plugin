95e7262cdba4eaf5b7965e81716ef7f2faa78cbf
John-Fries-J <w.bababooie@gmail.com>
2022-12-18 22:39:49 +0000

    Update Badges.plugin.js

--- Badges.plugin.js
+++ Badges.plugin.js
@@ -5,6 +5,7 @@
  * @version 1.0.1
  * @description Simple plugin that gives you all badges
  * @website https://johnfries.net
+ * @updateUrl https://raw.githubusercontent.com/JohnFries/BetterDiscordPlugins/master/Badges.plugin.js
  */
 
  module.exports = class Badges {
